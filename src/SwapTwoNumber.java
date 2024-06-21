d70760e299bcc19e5ce1b49b805c766d13965a86
avinash201 <avighalme20@gmail.com>
2024-06-21 12:03:13 +0530

    added programme to swap number

---
+++ src/SwapTwoNumber.java
@@ -0,0 +1,17 @@
+
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 20;
+		int b = 30;
+		int temp =0;
+		temp = a;
+		a=b;
+		b=temp;
+		System.out.println("a"+a+"b "+b);
+		
+	}
+
+}
